fd30b2e23bd93018975711e986d464e83a07ff7a
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-08-17 10:41:25 +0530

    Props in React

---
+++ module1.mjs
@@ -0,0 +1,4 @@
+import sc, { a, c } from "./module2.mjs";
+console.log(sc);
+console.log(a);
+console.log(c);
---
+++ module2.mjs
@@ -0,0 +1,7 @@
+const a = "Alex";
+const b = "John Doe";
+const c = "Mike";
+
+export default b;
+export { a };
+export { c };
