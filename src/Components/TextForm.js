3f9210b8e217daa0d91855fd6687168d537f3946
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-08-19 10:07:16 +0530

    Word counter using state of react

--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -16,23 +16,33 @@ export default function TextForm(props) {
   const [text, setText] = useState("Enter your text here");
 
   return (
-    <div>
-      <h1>{props.heading}</h1>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          id="myBox"
-          value={text}
-          rows="12"
-          onChange={onChange}
-        ></textarea>
+    <>
+      <div>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            value={text}
+            rows="12"
+            onChange={onChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={upperCase}>
+          UpperCase Conversion
+        </button>
+        <button className="btn btn-primary mx-3" type="reset" onClick={reset}>
+          Reset
+        </button>
       </div>
-      <button className="btn btn-primary" onClick={upperCase}>
-        UpperCase Conversion
-      </button>
-      <button className="btn btn-primary mx-3" type="reset" onClick={reset}>
-        Reset
-      </button>
-    </div>
+
+      <div className="container my-3">
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes read!</p>
+      </div>
+    </>
   );
 }
