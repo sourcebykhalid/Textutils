b1b28ef07dda11133bd9af5cdebaa2f282527f33
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-08-22 15:38:55 +0530

    Updates

---
+++ precache-manifest.11b2b196a9efe99e230ef36a8b243a78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cda380e1535fab95c4",
+    "url": "/textutils/static/css/main.867be247.chunk.css"
+  },
+  {
+    "revision": "42cda380e1535fab95c4",
+    "url": "/textutils/static/js/main.42cda380.chunk.js"
+  },
+  {
+    "revision": "927bd4bacd441db6510d",
+    "url": "/textutils/static/js/1.927bd4ba.chunk.js"
+  },
+  {
+    "revision": "11a3eb75ba923fb4e350",
+    "url": "/textutils/static/js/2.11a3eb75.chunk.js"
+  },
+  {
+    "revision": "6d8377050a97b48f66d5",
+    "url": "/textutils/static/js/runtime~main.6d837705.js"
+  },
+  {
+    "revision": "27f51deefe12898ed9b3b2175a9d4e1a",
+    "url": "/textutils/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactjs/precache-manifest.ef60fdfe04a99734e6769ac4910e17fc.js"
+  "/textutils/precache-manifest.11b2b196a9efe99e230ef36a8b243a78.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/reactjs/index.html", {
+workbox.routing.registerNavigationRoute("/textutils/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
